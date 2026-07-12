450a6ede265a353e8f3810f23e9e9080f3525256
Himaja Chanda <himajapatel0571@gmail.com>
2026-07-12 14:16:43 +0530

    intial commit

---
+++ pattern6.py
@@ -0,0 +1,33 @@
+n = int(input("Enter the number of rows: "))
+
+# Upper half
+for i in range(1, n + 1):
+    # Left stars
+    for j in range(i):
+        print("*", end="")
+
+    # Spaces
+    for j in range(2 * (n - i)):
+        print(" ", end="")
+
+    # Right stars
+    for j in range(i):
+        print("*", end="")
+
+    print()
+
+# Lower half
+for i in range(n - 1, 0, -1):
+    # Left stars
+    for j in range(i):
+        print("*", end="")
+
+    # Spaces
+    for j in range(2 * (n - i)):
+        print(" ", end="")
+
+    # Right stars
+    for j in range(i):
+        print("*", end="")
+
+    print()
