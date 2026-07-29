412a847fad46c362915dae4c34bdd4fe44a8fc9a
Himaja Chanda <himajapatel0571@gmail.com>
2026-07-29 17:51:01 +0530

    Intial commit

---
+++ pattern22.py
@@ -0,0 +1,13 @@
+n=int(input("n:"))
+for i in range(0,n):
+    for j in range(0,n-i-1):
+        print(" " ,end=" ")
+    ch=chr(ord('A')+i)
+    for j in range(2*i+1):
+        print(ch,end=" ")
+        
+        
+    
+    for j in range(0,n-i-1):
+        print(" " ,end=" ")
+    print()
